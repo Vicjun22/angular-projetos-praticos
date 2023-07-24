bad4ddae94f54b2ea3b4b8c170cc484ab84b2cd1
Victor Ross <vicjun22@gmail.com>
2023-07-24 18:37:31 -0300

    finalizando a parte lógica

--- projetos-praticos/src/app/pages/todo-list/todo-list.component.ts
+++ projetos-praticos/src/app/pages/todo-list/todo-list.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class TodoListComponent implements OnInit {
 
   public textoDeEntrada: string = '';
-  public listaDeAfazeres: Array<string> = [];
+  public listaDeAfazeres: Array<{checked: boolean, toDo: string}> = [];
   public conteudoArmazenamentoLocal: string | null = '';
 
   public ngOnInit(): void {
@@ -20,18 +20,36 @@ export class TodoListComponent implements OnInit {
   }
 
   public newToDo(): void {
-    this.listaDeAfazeres = [this.textoDeEntrada, ...this.listaDeAfazeres];
+    this.listaDeAfazeres = [{checked: false, toDo: this.textoDeEntrada}, ...this.listaDeAfazeres];
     window.localStorage.setItem('todo-list', JSON.stringify(this.listaDeAfazeres));
     this.textoDeEntrada = '';
   }
 
   public removeToDo(index: number): void {
-    this.listaDeAfazeres = this.listaDeAfazeres.filter((item: string) => item !== this.listaDeAfazeres[index]);
+    this.listaDeAfazeres = this.listaDeAfazeres.filter((item: Object) => item !== this.listaDeAfazeres[index]);
     window.localStorage.setItem('todo-list', JSON.stringify(this.listaDeAfazeres));
   }
 
   public removeAll(): void {
     this.listaDeAfazeres = [];
+    window.localStorage.setItem('todo-list', JSON.stringify(this.listaDeAfazeres));
+  }
+
+  public handleChangeCheckbox(index: number): void {
+    const checkboxChange: boolean = !this.listaDeAfazeres[index].checked;
+    const toDoChange: string = this.listaDeAfazeres[index].toDo
+    const listaDeAfazeresAtualizada = this.listaDeAfazeres.map((item: {checked: boolean, toDo: string}) => {
+      if (item !== this.listaDeAfazeres[index]) {
+        return item;
+      }
+      return {checked: checkboxChange, toDo: toDoChange}
+    })
+    const checkboxChecked: Array<{checked: boolean, toDo: string}> = listaDeAfazeresAtualizada
+      .filter((item: {checked: boolean, toDo: string}) => item.checked);
+    const checkboxUnchecked: Array<{checked: boolean, toDo: string}> = listaDeAfazeresAtualizada
+      .filter((item: {checked: boolean, toDo: string}) => !item.checked);
+
+    this.listaDeAfazeres = checkboxChecked.concat(checkboxUnchecked);
     window.localStorage.setItem('todo-list', JSON.stringify(this.listaDeAfazeres));
   }
 
