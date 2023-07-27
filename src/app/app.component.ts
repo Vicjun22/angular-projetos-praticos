9948546e01f7070c1290558fab10fbc9664f5c86
Victor Ross <vicjun22@gmail.com>
2023-07-27 09:13:10 -0300

    Upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'projetos-praticos';
+
+  constructor(public router: Router) {}
+  
+  public ngOnInit(): void {
+    // Redireciona para o primeiro desafio
+    // TODO => CRIAR PÁGINA DE BOAS VINDAS
+    // TODO => CRIAR BOTÃO VOLTAR NOS DESAFIOS PARA A PÁGINA DE BOAS VINDAS
+    this.router.navigate(['/todo-list']);
+  }
 }
